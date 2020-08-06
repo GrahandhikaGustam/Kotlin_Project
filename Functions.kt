c4898a9365dae8f486e3aa689b6c8bba08010cbb
GrahandhikaGustam <gustamagrahandhikaemail.com@gmail.com>
2020-08-07 00:17:14 +0700

    first commit

---
+++ Functions.kt
@@ -0,0 +1,6 @@
+fun main()
+{
+    val user = setUser("Gustam Putra", 16)
+    println(user)
+}
+fun setUser (name:String, age: Int) = "My name is $name, I like ride Bicycle and Play Game, and I'm $age years old"
---
+++ HelloKotlin.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    val name = "Grahandhika Gustam A.P"
+    val hobby = "Bermain Game, Bersepeda"
+
+    print("Nama saya ")
+    println(name)
+    print("Hobi saya ")
+    println(hobby)
+}
